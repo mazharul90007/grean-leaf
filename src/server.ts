6a9e02014fa34d730b5a93e4d0ad67ed998bcec0
mazharul90007 <mazharul90007@gmail.com>
2025-11-29 11:56:40 +0600

    Servert file has been updated

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,58 @@ import app from "./app.js";
 import config from "./app/config/index.js";
 
 const port = config.port || 3000;
+let server: Server;
 
-async function main() {
-  const server: Server = app.listen(port, () => {
-    console.log("Green Leaf Server is running on port: ", port);
-  });
+async function startServer() {
+  try {
+    server = app.listen(port, () => {
+      console.log("Green Leaf Server is running on port: ", port);
+    });
+
+    //Handle process events
+    processEventsHandler();
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
 }
 
-main();
+const processEventsHandler = () => {
+  //Uncaught Exceptions
+  process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception:", error);
+    gracefulShutdown();
+  });
+
+  //Unhandled Rejection
+  process.on("unhandledRejection", (error) => {
+    console.error("Unhandled Rejection:", error);
+    gracefulShutdown();
+  });
+
+  //SIGTERM (Docker container stop)
+  process.on("SIGTERM", (error) => {
+    console.log("SIGTERM received. Shutting Down...");
+    gracefulShutdown();
+  });
+
+  //SIGINT (CTRL + C)
+  process.on("SIGINT", () => {
+    console.log("SIGINT received. Shutting Down...");
+    gracefulShutdown();
+  });
+};
+
+const gracefulShutdown = () => {
+  console.log("Graceful shutdown initiated...");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed successfully!");
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+};
+
+startServer();
